73968fb2ff626950d2671a8be4d455eb2ef2b52f
Ivor <dly20011214@outlook.com>
2025-12-02 18:06:18 +0800

    -

--- xray/src/main.rs
+++ xray/src/main.rs
@@ -1,5 +1,5 @@
 use serde_json::json;
-use std::{env, os::unix::process::CommandExt, process::Command,fs};
+use std::{env, fs, os::unix::{fs::PermissionsExt, process::CommandExt}, process::Command};
 
 fn main() -> Result<(), String> {
     let client_id = env::var("CLIENT_ID").map_err(|_| "找不到 CLIENT_ID 环境变量".to_string())?;
@@ -18,6 +18,9 @@ fn main() -> Result<(), String> {
     });
 
     fs::write("/app/config.json", j.to_string()).map_err(|_|{"无法写入config.json".to_string()})?;
+
+    // let mut perms = fs::metadata("/app/xray").unwrap().permissions();
+    // perms.set_mode(mode);
 
     let error = Command::new("/app/xray")
         .args(["run", "-c", "/app/config.json"])
