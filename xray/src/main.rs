227568b1bb1ab80b13ffdf21505c632e13a7b638
Ivor <dly20011214@outlook.com>
2025-12-02 17:23:38 +0800

    -

---
+++ xray/src/main.rs
@@ -0,0 +1,27 @@
+use serde_json::json;
+use std::{env, os::unix::process::CommandExt, process::Command,fs};
+
+fn main() -> Result<(), String> {
+    let client_id = env::var("CLIENT_ID").map_err(|_| "找不到 CLIENT_ID 环境变量".to_string())?;
+    let j = json!({
+        "inbounds": [
+            {
+                "port": 80,
+                "listen": "0.0.0.0",
+                "protocol": "vless",
+                "settings": {"clients": [{"id": client_id}], "decryption": "none"},
+                "streamSettings": {"network": "xhttp", "security": "none"},
+                "xhttpSettings": {"path": "/", "mode": "stream-up"},
+            }
+        ],
+        "outbounds": [{"protocol": "freedom"}],
+    });
+
+    fs::write("/config.json", j.to_string()).map_err(|_|{"无法写入config.json".to_string()})?;
+
+    let error = Command::new("/xray")
+        .args(["run", "-c", "/config.json"])
+        .exec();
+
+    Err(format!("无法启动主程序:{}", error))
+}
